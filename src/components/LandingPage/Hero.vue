82ff959b74c9b1fa4a32b51f266e75b62bed3a08
Baker <baker@shamlan.sa>
2022-03-22 21:36:27 +0300

    Update Hero.vue

--- src/components/LandingPage/Hero.vue
+++ src/components/LandingPage/Hero.vue
@@ -9,8 +9,8 @@
             </div>
             <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
               <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-                <span class="block text-white">Take control of your</span>
-                <span class="block text-indigo-200">customer support</span>
+                <span class="block text-white">The Invention Hub</span>
+                <span class="block text-indigo-200">Where ideas come to life</span>
               </h1>
               <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
               <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
