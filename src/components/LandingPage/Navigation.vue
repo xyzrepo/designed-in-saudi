6f1cfe96f36577eb3058e9c9ff8629855446a639
Baker <baker@shamlan.sa>
2022-03-22 22:48:46 +0300

    Update Navigation.vue

--- src/components/LandingPage/Navigation.vue
+++ src/components/LandingPage/Navigation.vue
@@ -114,13 +114,13 @@
         </PopoverGroup>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
           <a
-            href="/signin"
+            href="/auth/signin"
             class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
           >
             Sign in
           </a>
           <a
-            href="/signup"
+            href="/auth/signup"
             class="ml-8 whitespace-nowrap inline-flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600 bg-origin-border px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white hover:from-purple-700 hover:to-indigo-700"
           >
             Sign up
