5e667d9e6285d2a625070bf9df18e1413c1ced66
Baker <baker@shamlan.sa>
2022-03-22 21:36:36 +0300

    Update Cards.vue

--- src/components/LandingPage/Cards.vue
+++ src/components/LandingPage/Cards.vue
@@ -6,8 +6,8 @@
     </div>
     <div class="relative max-w-7xl mx-auto">
       <div class="text-center">
-        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">From the blog</h2>
-        <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.</p>
+        <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">{{title}}</h2>
+        <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">{{subtitle}}</p>
       </div>
       <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
         <div v-for="post in posts" :key="post.title" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
@@ -59,8 +59,19 @@
   </div>
 </template>
 
-<script>
-const posts = [
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'From the blog'
+  },
+  subtitle: {
+    type: String,
+    default: 'Lorem ipsum dolor sit'
+  },
+  posts: {
+    type: Array,
+    default: [
   {
     title: 'Boost your conversion rate',
     href: '#',
@@ -116,12 +127,7 @@ const posts = [
     },
   },
 ]
+  }
+})
 
-export default {
-  setup() {
-    return {
-      posts,
-    }
-  },
-}
 </script>
