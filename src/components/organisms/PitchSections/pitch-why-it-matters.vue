0899de87b51e269455763f34df54d8dfb5caf447
Baker <baker@shamlan.sa>
2022-04-05 16:00:48 +0300

    Update pitch-why-it-matters.vue

--- src/components/organisms/PitchSections/pitch-why-it-matters.vue
+++ src/components/organisms/PitchSections/pitch-why-it-matters.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="py-24 overflow-hidden bg-white" style="background-image: url('flex-ui-assets/elements/pattern-white.svg'); background-position: center;">
+<section class="py-24 overflow-hidden bg-white" style="background-image: url('/flex-ui-assets/elements/pattern-white.svg'); background-position: center;">
         <div class="container px-4 mx-auto">
           <div class="flex flex-wrap -mx-4">
             <div class="w-full md:w-1/2 px-4 mb-20 lg:mb-0">
@@ -7,27 +7,27 @@
                 <h2 class="mb-8 text-4xl md:text-5xl font-heading font-bold text-gray-900 md:leading-15">Why Does It Matter?</h2>
                 <ul class="mb-8">
                   <li class="flex items-center mb-4">
-                    <img class="mr-3" src="flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
+                    <img class="mr-3" src="/flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
                     <span class="text-lg md:text-xl font-heading text-gray-500">Its difficult for individuals to manufacture an idea into a quality product.
                     </span>
                   </li>
                   <li class="flex items-center mb-4">
-                    <img class="mr-3" src="flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
+                    <img class="mr-3" src="/flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
                     <span class="text-lg md:text-xl font-heading text-gray-500">Its not easy for individuals to sell, market and deliver the product at scale.
                     </span>
                   </li>
                   <li class="flex items-center mb-4">
-                    <img class="mr-3" src="flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
+                    <img class="mr-3" src="/flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
                     <span class="text-lg md:text-xl font-heading text-gray-500">Lack of investment opportunities for individuals with conservative capital.
                     </span>
                   </li>
                   <li class="flex items-center mb-4">
-                    <img class="mr-3" src="flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
+                    <img class="mr-3" src="/flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
                     <span class="text-lg md:text-xl font-heading text-gray-500">Finding a sustainable secondary source of income is not an easy task.
                     </span>
                   </li>
                   <li class="flex items-center mb-4">
-                    <img class="mr-3" src="flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
+                    <img class="mr-3" src="/flex-ui-assets/elements/cta/checkbox-violet.svg" alt="">
                     <span class="text-lg md:text-xl font-heading text-gray-500">General lack of inventions in the sector of light manufactured consumer products in Saudi Arabia.
       
                     </span>
@@ -38,9 +38,9 @@
             </div>
             <div class="w-full md:w-1/2 px-4">
               <div class="relative max-w-max mx-auto">
-                <img class="absolute top-0 right-0 -mt-6 lg:-mt-12 -mr-6 lg:-mr-12 w-20 lg:w-auto z-10" src="flex-ui-assets/elements/circle3-green.svg" alt="">
-                <img class="absolute bottom-0 left-0 -mb-6 lg:-mb-10-ml-6 lg:-ml-12 w-20 lg:w-auto" src="flex-ui-assets/elements/dots3-yellow.svg" alt="">
-                <img class="relative" src="flex-ui-assets/elements/cta/photo-laptop-ph.png" alt="">
+                <img class="absolute top-0 right-0 -mt-6 lg:-mt-12 -mr-6 lg:-mr-12 w-20 lg:w-auto z-10" src="/flex-ui-assets/elements/circle3-green.svg" alt="">
+                <img class="absolute bottom-0 left-0 -mb-6 lg:-mb-10-ml-6 lg:-ml-12 w-20 lg:w-auto" src="/flex-ui-assets/elements/dots3-yellow.svg" alt="">
+                <img class="relative" src="/flex-ui-assets/elements/cta/photo-laptop-ph.png" alt="">
               </div>
             </div>
           </div>
