ef64005c3cfad00713008991f7a88759c3ff382a
Baker <baker@shamlan.sa>
2022-04-05 15:56:42 +0300

    Update pitch-market.vue

--- src/components/organisms/PitchSections/pitch-market.vue
+++ src/components/organisms/PitchSections/pitch-market.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="py-20 xl:py-24 bg-gray-900" style="background-image: url('flex-ui-assets/elements/pattern-dark2.svg'); background-position: center;">
+<section class="py-20 xl:py-24 bg-gray-900" style="background-image: url('/flex-ui-assets/elements/pattern-dark2.svg'); background-position: center;">
         <div class="container px-4 mx-auto">
           <span class="inline-block py-px px-2 mb-4 text-xs leading-5 text-violet-500 bg-violet-100 font-medium uppercase rounded-9xl">VALUABLE Statistics</span>
           <h3 class="mb-4 text-4xl md:text-4xl text-white font-bold tracking-tighter">Saudi Arabia Online Shopping Consumer Behavior in 2021</h3>
@@ -31,9 +31,9 @@
             </div>
             <div class="w-full md:w-1/2 px-4">
               <div class="relative mx-auto md:mr-0 max-w-max">
-                <img class="absolute z-10 -left-8 -top-8 w-28 md:w-auto text-yellow-400" src="flex-ui-assets/elements/circle3-green.svg" alt="">
-                <img class="absolute z-10 -right-7 -bottom-8 w-28 md:w-auto text-blue-500" src="flex-ui-assets/elements/dots3-yellow.svg" alt="">
-                <img class="rounded-3xl" src="flex-ui-assets/images/campaign-creators-pypeCEaJeZY-unsplash.jpg" alt="">
+                <img class="absolute z-10 -left-8 -top-8 w-28 md:w-auto text-yellow-400" src="/flex-ui-assets/elements/circle3-green.svg" alt="">
+                <img class="absolute z-10 -right-7 -bottom-8 w-28 md:w-auto text-blue-500" src="/flex-ui-assets/elements/dots3-yellow.svg" alt="">
+                <img class="rounded-3xl" src="/flex-ui-assets/images/campaign-creators-pypeCEaJeZY-unsplash.jpg" alt="">
               </div>
             </div>
           </div>
