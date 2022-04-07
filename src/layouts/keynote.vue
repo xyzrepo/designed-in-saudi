22800fe72e07266f89d341c7e96596378eb99d12
Baker <baker@shamlan.sa>
2022-04-07 09:04:10 +0300

    Update keynote.vue

--- src/layouts/keynote.vue
+++ src/layouts/keynote.vue
@@ -2,7 +2,7 @@
   <!-- <h1 class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
     The Other Layout
   </h1> -->
-  <button @click="switchDir" class="w-52 tracking-tighter rotate-45 leading-10 absolute bg-gradient-to-r from-primary-500 to-indigo-900 text-center font-extrabold text-2xl text-white z-10 top-6 -right-12">
+  <button @click="switchDir" class="overflow-hidden w-52 tracking-tighter rotate-45 leading-10 absolute bg-gradient-to-r from-primary-500 to-indigo-900 text-center font-extrabold text-2xl text-white z-10 top-6 -right-12">
     {{switchText}}
   </button>
   <router-view :dir="dir"></router-view>
