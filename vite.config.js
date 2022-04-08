013443accf62260ce5bc511f38b159fd2c9b0852
Baker <baker@shamlan.sa>
2022-04-08 05:10:09 +0300

    added VitaPWA

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import Pages from "vite-plugin-pages";
 import Layouts from 'vite-plugin-vue-layouts';
+import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({}),
     Components({
       dirs: [
         "src/components",
