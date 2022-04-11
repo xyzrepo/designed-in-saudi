5b6af708ed8e70e08b3bc9e0bb12d0dc67cd81e7
Baker <baker@shamlan.sa>
2022-04-11 09:21:56 +0300

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,108 +1,34 @@
-<!--
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
     <main>
       <!-- Hero section -->
-      <Hero />
+      <Hero 
+        :title="content.intro.title"
+        :subtitle="content.intro.subtitle"
+        :description="content.intro.description"
+      />
 
-      <Cards title="Our Projects"/>
-      <BlogCards/>
+      <!-- <Cards title="Our Projects"/> -->
+      <!-- <BlogGrid/> -->
 
       <!-- Logo Cloud -->
-      <ShowcaseLogos :altStyle="true" title="Trusted by over 5 very average small businesses" />
+      <!-- <ShowcaseLogos :altStyle="true" title="Trusted by over 5 very average small businesses" /> -->
 
       <!-- Alternating Feature Sections -->
       <!-- <AlternatingFeatureSection /> -->
 
       <!-- Gradient Feature Section -->
-      <FeaturesWithGradient :features="features"/>
+      <!-- <FeaturesWithGradient :features="features"/> -->
 
       <!-- Stats section -->
-      <Stats :metrics="metrics"/>
+      <!-- <Stats :metrics="metrics"/> -->
 
       <!-- CTA Section -->
-      <CTA />
+      <!-- <CTA /> -->
     </main>
 </template>
 
 <script setup>
-import {
-  AnnotationIcon,
-  ChatAlt2Icon,
-  ChatAltIcon,
-  DocumentReportIcon,
-  HeartIcon,
-  InboxIcon,
-  MenuIcon,
-  PencilAltIcon,
-  QuestionMarkCircleIcon,
-  ReplyIcon,
-  SparklesIcon,
-  TrashIcon,
-  UsersIcon,
-  XIcon,
-} from '@heroicons/vue/outline'
-
-const features = [
-  {
-    name: 'Unlimited Inboxes',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: InboxIcon,
-  },
-  {
-    name: 'Manage Team Members',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: UsersIcon,
-  },
-  {
-    name: 'Spam Report',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: TrashIcon,
-  },
-  {
-    name: 'Compose in Markdown',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: PencilAltIcon,
-  },
-  {
-    name: 'Team Reporting',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: DocumentReportIcon,
-  },
-  {
-    name: 'Saved Replies',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ReplyIcon,
-  },
-  {
-    name: 'Email Commenting',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: ChatAltIcon,
-  },
-  {
-    name: 'Connect with Customers',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: HeartIcon,
-  },
-]
-const metrics = [
-  { id: 1, stat: '8K+', emphasis: 'Companies', rest: 'use laoreet amet lacus nibh integer quis.' },
-  { id: 2, stat: '25K+', emphasis: 'Countries around the globe', rest: 'lacus nibh integer quis.' },
-  { id: 3, stat: '98%', emphasis: 'Customer satisfaction', rest: 'laoreet amet lacus nibh integer quis.' },
-  { id: 4, stat: '12M+', emphasis: 'Issues resolved', rest: 'lacus nibh integer quis.' },
-]
+import { computed } from 'vue'
+import { en } from '@/components/composables/useContent.js';
+const content = computed(() => en)
 </script>
