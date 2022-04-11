8b575fd7ff556f6afbfab22f0a7724da1fe9f598
Baker <baker@shamlan.sa>
2022-04-11 09:53:04 +0300

    Update Partners.vue

--- src/pages/Partners.vue
+++ src/pages/Partners.vue
@@ -5,12 +5,12 @@
     :subtitle="content.platform.intro.subtitle"
   />
   <nav class="h-full overflow-y-auto" aria-label="Directory">
-    <div v-for="partner in content.partners.network" :key="partner.role" class="relative">
-      <div class="z-20 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
+    <div v-for="partner in content.partners.network.filter(p => ['Design','Manufacturing'].includes(p.role))" :key="partner.role" class="relative">
+      <div class="z-10 sticky top-0 border-t border-b border-gray-200 bg-gray-50 px-6 py-1 text-sm font-medium text-gray-500">
         <h3>{{ partner.role }}</h3>
       </div>
       <ul role="list" class="relative z-0 divide-y divide-gray-200">
-        <li v-for="person in content.partners.network.filter(p => partner.role === p.role)" :key="person.id" class="bg-white">
+        <li v-for="person in content.partners.network.filter(p => p.role == role)" :key="person.id" class="bg-white">
           <div class="relative px-6 py-5 flex items-center space-x-3 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
             <div class="flex-shrink-0">
               <img class="h-10 w-10 rounded-full" :src="person.imageUrl" alt="" />
@@ -36,72 +36,9 @@
 
 <script setup>
 import content from '@/components/composables/useContent';
-const directory = {
-  S: [
-    {
-      id: 19,
-      name: 'Courtney Samuels',
-      role: 'Designer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 20,
-      name: 'Tom Simpson',
-      role: 'Director, Product Development',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  ],
-  T: [
-    {
-      id: 21,
-      name: 'Floyd Thompson',
-      role: 'Principal Designer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 22,
-      name: 'Leonard Timmons',
-      role: 'Senior Designer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 23,
-      name: 'Whitney Trudeau',
-      role: 'Copywriter',
-      imageUrl:
-        'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  ],
-  W: [
-    {
-      id: 24,
-      name: 'Kristin Watson',
-      role: 'VP, Human Resources',
-      imageUrl:
-        'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      id: 25,
-      name: 'Emily Wilson',
-      role: 'VP, User Experience',
-      imageUrl:
-        'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  ],
-  Y: [
-    {
-      id: 26,
-      name: 'Emma Young',
-      role: 'Senior Front-end Developer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  ],
-}
+const directory = [
+    
+  ]
 </script>
 <route>
 {
