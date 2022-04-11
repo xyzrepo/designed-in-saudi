05085629827d47126ca26ddb996296b763ec0a79
Baker <baker@shamlan.sa>
2022-04-11 09:21:11 +0300

    Update PitchValueProposition.vue

--- src/components/organisms/PitchSections/PitchValueProposition.vue
+++ src/components/organisms/PitchSections/PitchValueProposition.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="py-24 bg-gray-900" :style="`background-image: url(${pattern}); background-position: center;`">
+<section class="py-24 bg-gray-900" :style="`background-image: url(${backgroundPattern}); background-position: center;`">
         <div class="container px-4 mx-auto">
           <div class="xl:max-w-4xl mb-12 mx-auto text-center">
             
@@ -18,7 +18,7 @@
             </div>
             <div class="w-full lg:w-1/3 px-4 mb-8 lg:mb-0">
               <!-- <img class="inline-flex mx-auto rounded-3xl" :src="valuePropositionImage" alt="lightbulb"> -->
-                <img class="inline-flex md:w-full mx-auto h-auto mt-10  rounded-3xl" src="/src/assets/images/pitch/value-proposition.jpg" alt="lightbulb" width="492" height="600">
+                <img class="md:w-full mx-auto h-auto rounded-3xl" src="/src/assets/images/pitch/value-proposition.jpg" alt="lightbulb" width="400" height="600">
             </div>
             <div class="w-full lg:w-1/3 lg:pt-6 px-4">
               <div class="p-8 text-center hover:bg-gray-700 rounded-md hover:shadow-xl transition duration-200" v-for="(item, index) in items.slice(-Math.ceil(items.length / 2))" :key="index">
@@ -34,8 +34,6 @@
       </section>
 </template>
 <script setup>
-// import { valueProposition as valuePropositionImage } from '~/components/composables/usePitchImages';
-// import { CubeIcon, ClipboardListIcon, CurrencyDollarIcon, CreditCardIcon } from '@heroicons/vue/outline'
-import pattern from '~/assets/images/elements/pattern-dark2.svg'
-defineProps({ title: String, subtitle: String, items: Array })
+import backgroundPattern from '~/assets/images/elements/pattern-dark2.svg'
+defineProps({ title: String,subtitle: String, items: Array })
 </script>
