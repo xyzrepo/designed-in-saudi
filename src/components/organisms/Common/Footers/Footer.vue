c72da0f44eeda0120e5006cb26e4ee39ad52a3d6
Baker <baker@shamlan.sa>
2022-04-13 00:48:24 +0300

    Update Footer.vue

--- src/components/organisms/Common/Footers/Footer.vue
+++ src/components/organisms/Common/Footers/Footer.vue
@@ -3,14 +3,14 @@
   <footer class="bg-gray-50">
     <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
       <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-        <div v-for="item in navigation.main" :key="item.name" class="px-5 py-2">
+        <div v-for="item in navigation" :key="item.name" class="px-5 py-2">
           <a :href="item.href" class="text-2xl gradient font-extrabold text-gray-500 active:text-gray-900 hover:text-primary-900 underline-offset-8 decoration-8 hover:underline">
             {{ item.name }}
           </a>
         </div>
       </nav>
       <div class="mt-8 flex justify-center">
-        <a v-for="item in navigation.social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-primary-500 px-1">
+        <a v-for="item in socials" :key="item.name" :href="item.href" class="text-gray-400 hover:text-primary-500 px-1">
           <span class="sr-only">{{ item.name }}</span>
           <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
         </a>
@@ -23,15 +23,16 @@
 <script setup>
 import { socials } from '~/components/composables';
 
-const navigation = {
-  main: [
-    { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
-    { name: 'Press', href: '/press' },
-    // { name: 'Accessibility', href: '#' },
-    // { name: 'Partners', href: '/partners' },
-  ],
-  social: socials,
-}
+// const navigation = {
+//   main: [
+//     { name: 'About', href: '#' },
+//     { name: 'Blog', href: '#' },
+//     { name: 'Jobs', href: '#' },
+//     { name: 'Press', href: '/press' },
+//     // { name: 'Accessibility', href: '#' },
+//     // { name: 'Partners', href: '/partners' },
+//   ],
+//   social: socials,
+// }
+defineProps({ navigation: Array })
 </script>
