7291ecc8d4100534220a23eba2bfd61096f1a433
Baker <baker@shamlan.sa>
2022-04-12 07:25:09 +0300

    Update Hero.vue

--- src/components/organisms/Common/Hero/Hero.vue
+++ src/components/organisms/Common/Hero/Hero.vue
@@ -10,9 +10,9 @@
       <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
         <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
           <span class="block text-white">{{title}}</span>
-          <span class="block text-indigo-200">{{subtitle}}</span>
+          <span class="block text-2xl sm:text-4xl text-indigo-200">{{subtitle}}</span>
         </h1>
-        <p class="mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl">{{description}}</p>
+        <p class="mt-6 max-w-lg mx-auto text-center text-2xl font-light text-indigo-200 sm:max-w-3xl">{{description}}</p>
         <div class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
           <!-- <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
             <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"> Get started </a>
