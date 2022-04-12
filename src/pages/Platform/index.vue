ee41e2812c7c9dd3a5fcc110ce373601fb040203
Baker <baker@shamlan.sa>
2022-04-12 07:27:34 +0300

    Update index.vue

--- src/pages/Platform/index.vue
+++ src/pages/Platform/index.vue
@@ -2,7 +2,7 @@
 <div class="snap-y snap-mandatory overflow-scroll">
     <BrandedHeading 
         :title="content.platform.intro.title"
-        :subtitle="content.platform.intro.subtitle"
+        :subtitle="content.platform.subtitle"
         image="https://images.unsplash.com/photo-1516331138075-f3adc1e149cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1508&q=80"
     />
     <FeaturesGrid
