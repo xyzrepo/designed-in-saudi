6924c7243b4a8aa3ed4e45f90123f3ad75e0dbc0
Baker <baker@shamlan.sa>
2022-04-13 03:48:21 +0300

    Update ideas.vue

--- src/pages/Dashboard/ideas.vue
+++ src/pages/Dashboard/ideas.vue
@@ -1,6 +1,14 @@
 <template>
-    Za Dashboard
+    <CardHeading title="Manage Ideas" button="Create a new idea" :action="newIdea"/>
+    <Empty v-if="!ideas.length" name="idea" :action="newIdea" />
+    <List v-else="ideas.length"/>
+    <!-- {{ ideas }} -->
 </template>
+<script setup>
+import { ref } from 'vue'
+const ideas = ref([])
+const newIdea = () => ideas.value.push('New IDEA')
+</script>
 <route>
 {
     meta: {
