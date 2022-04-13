fffa19e981742d8fe54c004892ebcf90faf0bb59
Baker <baker@shamlan.sa>
2022-04-13 03:48:04 +0300

    Create Empty.vue

---
+++ src/components/Base/Listings/Empty.vue
@@ -0,0 +1,19 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class="text-center">
+    <component :is="icon" class="mx-auto h-12 w-12 text-gray-400 stroke-1" />
+    <h3 class="mt-2 text-sm font-medium text-gray-900">No {{name}}s</h3>
+    <p class="mt-1 text-sm text-gray-500">Get started by creating a new {{name}}.</p>
+    <div class="mt-6">
+      <button @click="action" type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 capitalize">
+        <PlusIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+        New {{name}}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { PlusIcon, FolderAddIcon } from '@heroicons/vue/outline'
+defineProps({ name: String, action: Object, icon: { type: String, default: FolderAddIcon } })
+</script>
