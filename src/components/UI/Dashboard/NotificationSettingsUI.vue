1c48b63d80a87b9cf87fa5860fa9a9a4170c4343
Baker <baker@shamlan.sa>
2022-04-15 01:35:41 +0300

    Create NotificationSettingsUI.vue

---
+++ src/components/UI/Dashboard/NotificationSettingsUI.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="pt-8">
+        <div>
+            <h3 class="text-lg leading-6 font-medium text-gray-900">Notifications</h3>
+            <p class="mt-1 text-sm text-gray-500">We'll always let you know about important changes, but you pick what else you want to hear about.</p>
+        </div>
+        <div class="mt-6">
+            <fieldset>
+                <legend class="text-base font-medium text-gray-900">By Email</legend>
+                <div class="mt-4 space-y-4">
+                    <div class="relative flex items-start">
+                        <div class="flex items-center h-5">
+                            <input id="comments" name="comments" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                        </div>
+                        <div class="ml-3 text-sm">
+                            <label for="comments" class="font-medium text-gray-700">Comments</label>
+                            <p class="text-gray-500">Get notified when someones posts a comment on a posting.</p>
+                        </div>
+                    </div>
+                    <div class="relative flex items-start">
+                        <div class="flex items-center h-5">
+                            <input id="candidates" name="candidates" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                        </div>
+                        <div class="ml-3 text-sm">
+                            <label for="candidates" class="font-medium text-gray-700">Candidates</label>
+                            <p class="text-gray-500">Get notified when a candidate applies for a job.</p>
+                        </div>
+                    </div>
+                    <div class="relative flex items-start">
+                        <div class="flex items-center h-5">
+                            <input id="offers" name="offers" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                        </div>
+                        <div class="ml-3 text-sm">
+                            <label for="offers" class="font-medium text-gray-700">Offers</label>
+                            <p class="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
+                        </div>
+                    </div>
+                </div>
+            </fieldset>
+            <fieldset class="mt-6">
+                <div>
+                    <legend class="text-base font-medium text-gray-900">Push Notifications</legend>
+                    <p class="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
+                </div>
+                <div class="mt-4 space-y-4">
+                    <div class="flex items-center">
+                        <input id="push-everything" name="push-notifications" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                        <label for="push-everything" class="ml-3 block text-sm font-medium text-gray-700"> Everything </label>
+                    </div>
+                    <div class="flex items-center">
+                        <input id="push-email" name="push-notifications" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                        <label for="push-email" class="ml-3 block text-sm font-medium text-gray-700"> Same as email </label>
+                    </div>
+                    <div class="flex items-center">
+                        <input id="push-nothing" name="push-notifications" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+                        <label for="push-nothing" class="ml-3 block text-sm font-medium text-gray-700"> No push notifications </label>
+                    </div>
+                </div>
+            </fieldset>
+        </div>
+    </div>
+</template>
