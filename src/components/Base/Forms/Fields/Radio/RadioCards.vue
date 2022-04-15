28983e7161d34fa642470177a9659951af9e8139
Baker <baker@shamlan.sa>
2022-04-15 14:02:35 +0300

    Update RadioCards.vue

--- src/components/Base/Forms/Fields/Radio/RadioCards.vue
+++ src/components/Base/Forms/Fields/Radio/RadioCards.vue
@@ -1,22 +1,22 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-    <RadioGroup v-model="selectedOption" class="my-10">
+    <RadioGroup v-model="selectedOption" class="my-6">
         <RadioGroupLabel class="pt-8 text-2xl font-medium text-gray-900">{{label}}</RadioGroupLabel>
 
-        <div class="mt-1 pt-4 flex text-2xl text-gray-900 sm:mt-0 sm:col-span-2" v-if="selectedOption">
+        <div class="mt-1 pt-4 flex text-xl text-gray-900 sm:mt-0 sm:col-span-2" v-if="selectedOption">
           <span class="flex-grow">{{selectedOption?.description}}</span>
           <span class="ml-4 flex-shrink-0">
             <!-- <button type="button" class="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Update</button> -->
             <SecondaryButton @click="selectedOption = null">
-                <RefreshIcon class="w-5 h-5 stroke-2"/>
+                <RefreshIcon class="w-5 h-5"/>
                 Update
             </SecondaryButton>
           </span>
         </div>
 
-        <div class="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-1 lg:grid-cols-1 sm:gap-x-4" v-show="!selectedOption">
-            <RadioGroupOption as="template" v-for="option in options" :key="option.id" :value="option" v-slot="{ checked, active }">
-                <div :class="[checked ? 'border-transparent' : 'border-gray-300', active ? 'border-indigo-500 ring-2 ring-indigo-500' : '', 'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none']">
+        <div :class="`mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-${grid ? '2' : '1'} lg:grid-cols-${grid ? '3' : '1'} sm:gap-x-4`" v-show="selectedOption === null">
+            <RadioGroupOption as="span" v-for="option in options" :key="option.id" :value="option" v-slot="{ checked, active }">
+                <div :class="[checked ? 'border-transparent' : 'border-gray-300', active ? 'border-indigo-500 ring-2 ring-indigo-500' : '', 'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none']" @click="emit('update:modelValue',selectedOption)">
                     <div class="flex-1 flex">
                         <div class="flex flex-col">
                             <RadioGroupLabel v-if="option?.name" as="span" class="block text-xl font-semibold text-gray-900">
@@ -44,6 +44,10 @@ import { RadioGroup, RadioGroupDescription, RadioGroupLabel, RadioGroupOption } 
 import { RefreshIcon } from '@heroicons/vue/outline'
 import { CheckCircleIcon } from '@heroicons/vue/solid';
 defineProps({
+    grid: {
+        type: Boolean,
+        default: true
+    },
     label: { 
         type: String,
         // default: 'Select an option'
@@ -57,4 +61,5 @@ defineProps({
     }
 })
 const selectedOption = ref(null)
+const emit = defineEmits(['update:modalValue'])
 </script>
