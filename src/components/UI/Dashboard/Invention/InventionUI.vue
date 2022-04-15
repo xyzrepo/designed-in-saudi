9e1c22a80205c24017f1d1b2835a351c23950ff8
Baker <baker@shamlan.sa>
2022-04-15 14:03:42 +0300

    Update InventionUI.vue

--- src/components/UI/Dashboard/Invention/InventionUI.vue
+++ src/components/UI/Dashboard/Invention/InventionUI.vue
@@ -4,7 +4,7 @@
     {{ steps[step]?.name || 'Complete' }}
     </Title> -->
     <span id="top"></span>
-
+    
     <InventionStep :step="0" title="Ready to invent?" subtitle="Start creating your first invention today." class="text-center pt-24"
         :action="{ name: `Let's get started` , fn: nextStep }"
     />
@@ -14,19 +14,29 @@
     </InventionStep>
 
     <InventionStep :step="2" title="Category" :action="{ name: `Continue` , fn: nextStep }">
-        <InventionCategory v-model="formData.category" :categories="categories" />
+        <RadioCards :grid="true" v-model="formData.category" label="Category" :options="categories" />
+        <RadioCards
+            v-model="formData.subcategory"
+            label="Subcategory"
+            :options="formData.category.subcategories" 
+            v-if="formData.category?.subcategories?.length > 0"
+            />
     </InventionStep>
 
-    <InventionStep :step="3" title="Sub Category" :action="{ name: `Continue` , fn: nextStep }">
-        <InventionCategory v-if="formData.category?.subcategories?.length > 0" v-model="formData.subcategory" :categories="formData.category.subcategories" />
+    <!-- <InventionStep :step="3" title="Sub Category" :action="{ name: `Continue` , fn: nextStep }">
+        <RadioCards 
+            v-model="formData.subcategory"
+            :options="formData.category.subcategories" 
+            v-if="formData.category?.subcategories?.length > 0"
+            />
+    </InventionStep> -->
         <!-- <InventionCategory v-model="formData.subcategory" :categories="formData.category.subcategories" /> -->
-    </InventionStep>
     
-    <InventionStep :step="4" title="Patent and Disclosure" subtitle="invention.welcome.subtitle #4">
+    <InventionStep :step="3" title="Patent and Disclosure" subtitle="invention.welcome.subtitle #4">
         <InventionPatent />
     </InventionStep>
 
-    <InventionStep :step="5" title="Privacy Setting" subtitle="Please choose the privacy level you require">
+    <InventionStep :step="4" title="Privacy Setting" subtitle="Please choose the privacy level you require">
         <PrivacySettingField v-model="formData['privacy']"/>
     </InventionStep>
 
@@ -38,7 +48,6 @@
 </template>
 <script setup>
 import useInvention, { steps, step, formData, categories, nextStep } from '@composables/useInvention';
-import InventionPatent from './InventionPatent.vue';
 const submit = () => { console.log('complete') }
 // const updateFormData = (field) => {
 //     console.log('field:', field);
