bba375005e15350ec86d1bf623256158cb2cea52
Baker <baker@shamlan.sa>
2022-04-15 12:30:46 +0300

    Update InventionUI.vue

--- src/components/UI/Dashboard/Invention/InventionUI.vue
+++ src/components/UI/Dashboard/Invention/InventionUI.vue
@@ -1,56 +1,50 @@
 <template>
-<InventionWizard>
-    <Title>
+<InventionWizard >
+    <!-- <Title>
     {{ steps[step]?.name || 'Complete' }}
-    </Title>
+    </Title> -->
+    <span id="top"></span>
 
-    <InventionStep :step="0" title="Ready to invent?" subtitle="Start creating your first invention today." class="text-center"
+    <InventionStep :step="0" title="Ready to invent?" subtitle="Start creating your first invention today." class="text-center pt-24"
         :action="{ name: `Let's get started` , fn: nextStep }"
     />
     
-    <InventionStep :step="1" title="Invention Name" subtitle="Please give your invention a name and a description.">
+    <InventionStep :step="1" title="Invention Information" subtitle="Tell us a little bit about your invention.">
        <InventionForm />
     </InventionStep>
 
-    <InventionStep :step="2" title="Invention Category" :action="{ name: `Continue` , fn: nextStep }">
-        <InventionCategory v-model="formData['category']" :categories="categories" />
+    <InventionStep :step="2" title="Category" :action="{ name: `Continue` , fn: nextStep }">
+        <InventionCategory v-model="formData.category" :categories="categories" />
+    </InventionStep>
+
+    <InventionStep :step="3" title="Sub Category" :action="{ name: `Continue` , fn: nextStep }">
+        <InventionCategory v-if="formData.category?.subcategories?.length > 0" v-model="formData.subcategory" :categories="formData.category.subcategories" />
+        <!-- <InventionCategory v-model="formData.subcategory" :categories="formData.category.subcategories" /> -->
     </InventionStep>
     
-    <InventionStep :step="3" title="invention.welcome.title #4" subtitle="invention.welcome.subtitle #4"/>
+    <InventionStep :step="4" title="Patent and Disclosure" subtitle="invention.welcome.subtitle #4">
+        <InventionPatent />
+    </InventionStep>
 
-    <InventionStep :step="4" title="Privacy Setting" subtitle="Please choose the privacy level you require">
+    <InventionStep :step="5" title="Privacy Setting" subtitle="Please choose the privacy level you require">
         <PrivacySettingField v-model="formData['privacy']"/>
     </InventionStep>
 
-    <InventionStep :step="steps.length" title="Review Your Submission" :action="{ name: 'Submit', fn: log('submit') }">
+    <InventionStep :step="steps.length" title="Review Your Submission" :action="{ name: 'Submit', fn: () => submit }">
         <InventionReview :data="formData"/>
     </InventionStep>
-    
+    <span id="bottom"></span>
 </InventionWizard>
 </template>
 <script setup>
-// import { ref, unref, reactive } from 'vue'
-import { invention } from '@content'
 import useInvention, { steps, step, formData, categories, nextStep } from '@composables/useInvention';
-function log(s) { console.log(s) }
-// const lastStep = computed(() => step.value === steps.value.length-1)
-// const lastStepConfirmed = computed(() => step.value === steps.value.length)
-// const emit = defineEmits(['complete'])
-// const step = useInvention.step
-// const formData = useInvention.formData
-// const category = 
-// const { formData } = useInvention()
-invention.welcome = {
-    title: 'Ready to invent?',
-    subtitle: 'Start creating your first invention today.'
-}
-const updateFormData = (field) => {
-    console.log('field:', field);
-    return (payload) => { 
-        formData[field] = payload;
-    }
-    // payload => { console.log(formData.value); formData.value[field] = payload; step.value++ }
-}
-const choose = (category) => { console.log(category); formData.value.category = category.slug; step.value++ }
-// console.log(invention);
+import InventionPatent from './InventionPatent.vue';
+const submit = () => { console.log('complete') }
+// const updateFormData = (field) => {
+//     console.log('field:', field);
+//     return (payload) => { 
+//         formData[field] = payload;
+//     }
+//     // payload => { console.log(formData.value); formData.value[field] = payload; step.value++ }
+// }
 </script>
