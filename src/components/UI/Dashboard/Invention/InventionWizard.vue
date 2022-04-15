9b26f621697b550d53a9e62e3cdd7bd031055abd
Baker <baker@shamlan.sa>
2022-04-15 12:30:39 +0300

    Update InventionWizard.vue

--- src/components/UI/Dashboard/Invention/InventionWizard.vue
+++ src/components/UI/Dashboard/Invention/InventionWizard.vue
@@ -1,22 +1,20 @@
 <template>
     <PageHeading 
-        
         :actions="[
             validStep
                 ? { name: 'Previous', action: prevStep }
-                : { name: 'Continue Editing', action: prevStep },
+                : { name: 'Back', action: prevStep },
             validStep
                 ? { name: 'Next', action: nextStep, primary: true } 
-                : { name: 'Confirm and Submit', action: nextStep, primary: true },
+                : { name: 'Confirm and Submit', action: scrollToSubmit, primary: true },
             
         ]">
         
         <template #actions v-if="step === 0">
-            <!-- hide default action buttons -->
-            <span> </span>
+            <span></span>
         </template>
 
-        <template #breadcrumb>
+        <template #breadcrumb >
             <StepsCircle :steps="steps"/>
         </template>
 
@@ -30,5 +28,25 @@
 </template>
 <script setup>
 import { step, steps, nextStep, prevStep } from '@composables/useInvention';
+const scrollToSubmit = () => {
+    let element = document.getElementById("bottom");
+    console.log(element);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
 const validStep = computed(() => step.value < steps.value.length)
+const props = defineProps({ 
+    steps: {
+        type: Array, 
+        default: [
+            { name: 'Step 1', status: 'upcoming' },
+            { name: 'Step 2' },
+            { name: 'Step 3' },
+            { name: 'Step 4' },
+            { name: 'Step 5' },
+            { name: 'Step 6' },
+        ]
+    }
+})
+steps.value = props.steps
+steps.value[0].status = 'current'
 </script>
