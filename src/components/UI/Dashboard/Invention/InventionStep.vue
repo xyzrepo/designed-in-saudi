ff3eb9d9e4afcf4c8212ee51270a04f255e2c4d5
Baker <baker@shamlan.sa>
2022-04-15 12:30:23 +0300

    Update InventionStep.vue

--- src/components/UI/Dashboard/Invention/InventionStep.vue
+++ src/components/UI/Dashboard/Invention/InventionStep.vue
@@ -2,7 +2,11 @@
     <div class="bg-gray-50" v-show="step === currentStep">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:py-6 lg:px-8">
       <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-        <span class="block">{{title}}</span>
+        
+        <span class="block">
+          <!-- <span>{{`${steps[step]?.name ? steps[step].name+': ' : ''}`}}</span> -->
+          {{title}}
+        </span>
         <span class="block text-2xl font-light">{{subtitle}}</span>
       </h2>
       
@@ -10,7 +14,7 @@
       <div class="mt-8 flex justify-center">
         
         <div class="mr-3 inline-flex">
-            <SecondaryButton v-if="step > 0" @click="prevStep">Back</SecondaryButton>
+            <SecondaryButton v-if="step > 0" @click="prevStep">Go Back</SecondaryButton>
           <!-- <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200"> Learn more </a> -->
         </div>
 
@@ -24,9 +28,9 @@
   </div>
 </template>
 <script setup>
-import { step as currentStep, prevStep, nextStep } from '@composables/useInvention';
-defineProps({
-    step: Number,
+import { steps, step as currentStep, prevStep, nextStep } from '@composables/useInvention';
+const props = defineProps({
+    step: { type: Number, default: -1 },
     title: String,
     subtitle: String,
     action: { type: Object, default: { name: 'Continue', fn: nextStep } }
