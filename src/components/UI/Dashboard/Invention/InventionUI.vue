4eb0ae9c3ab374d8758e552378835e70c51a3318
Baker <baker@shamlan.sa>
2022-04-15 05:21:32 +0300

    Update InventionUI.vue

--- src/components/UI/Dashboard/Invention/InventionUI.vue
+++ src/components/UI/Dashboard/Invention/InventionUI.vue
@@ -3,36 +3,28 @@
     <Title>
     {{ steps[step]?.name || 'Complete' }}
     </Title>
-    <InventionWelcome v-show="step === 0" 
-        :title="invention.welcome.title" 
-        :subtitle="invention.welcome.subtitle" 
-        :action="nextStep"
+
+    <InventionStep :step="0" title="Ready to invent?" subtitle="Start creating your first invention today." class="text-center"
+        :action="{ name: `Let's get started` , fn: nextStep }"
     />
     
-    <InventionCategory v-show="step === 1" v-model="formData['category']" :categories="categories" />
+    <InventionStep :step="1" title="Invention Name" subtitle="Please give your invention a name and a description.">
+       <InventionForm />
+    </InventionStep>
 
-    <InventionWelcome v-show="step === 2" 
-        title="invention.welcome.title #3"
-        subtitle="invention.welcome.subtitle #3"
-        :action="() => step++"
-    />
-    <InventionWelcome v-show="step === 3" 
-        title="invention.welcome.title #4"
-        subtitle="invention.welcome.subtitle #4"
-        :action="() => step++"
-    />
+    <InventionStep :step="2" title="Invention Category" :action="{ name: `Continue` , fn: nextStep }">
+        <InventionCategory v-model="formData['category']" :categories="categories" />
+    </InventionStep>
+    
+    <InventionStep :step="3" title="invention.welcome.title #4" subtitle="invention.welcome.subtitle #4"/>
 
-    <InventionWelcome v-show="lastStep" 
-        title="invention.welcome.title #last"
-        subtitle="invention.welcome.subtitle #last"
-        :action="() => emit('complete')"
-    />
+    <InventionStep :step="4" title="Privacy Setting" subtitle="Please choose the privacy level you require">
+        <PrivacySettingField v-model="formData['privacy']"/>
+    </InventionStep>
 
-    <InventionWelcome v-show="lastStepConfirmed" 
-        title="invention.welcome.title #confirmed"
-        subtitle="invention.welcome.subtitle #confirmed"
-        :action="() => emit('complete')"
-    />
+    <InventionStep :step="steps.length" title="Review Your Submission" :action="{ name: 'Submit', fn: log('submit') }">
+        <InventionReview :data="formData"/>
+    </InventionStep>
     
 </InventionWizard>
 </template>
@@ -40,14 +32,14 @@
 // import { ref, unref, reactive } from 'vue'
 import { invention } from '@content'
 import useInvention, { steps, step, formData, categories, nextStep } from '@composables/useInvention';
-const lastStep = computed(() => step.value === steps.value.length-1)
-const lastStepConfirmed = computed(() => step.value === steps.value.length)
-const emit = defineEmits(['complete'])
+function log(s) { console.log(s) }
+// const lastStep = computed(() => step.value === steps.value.length-1)
+// const lastStepConfirmed = computed(() => step.value === steps.value.length)
+// const emit = defineEmits(['complete'])
 // const step = useInvention.step
 // const formData = useInvention.formData
 // const category = 
 // const { formData } = useInvention()
-console.log(formData);
 invention.welcome = {
     title: 'Ready to invent?',
     subtitle: 'Start creating your first invention today.'
@@ -56,7 +48,6 @@ const updateFormData = (field) => {
     console.log('field:', field);
     return (payload) => { 
         formData[field] = payload;
-        step++
     }
     // payload => { console.log(formData.value); formData.value[field] = payload; step.value++ }
 }
