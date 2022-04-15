1bb76e05a2d6b6307612f5a02d568a35f21e97f5
Baker <baker@shamlan.sa>
2022-04-15 12:29:54 +0300

    Update InventionForm.vue

--- src/components/UI/Dashboard/Invention/InventionForm.vue
+++ src/components/UI/Dashboard/Invention/InventionForm.vue
@@ -1,23 +1,23 @@
 <template>
-<div class="space-y-6">
+<div class="space-y-6 py-6">
     <div>
-        <label for="name" class="block text-sm font-medium text-gray-700">Invention Name</label>
+        <label for="name" class="block text-2xl font-medium text-gray-700">Invention Name</label>
         <div class="mt-1">
-            <input v-model="formData.name" type="text" name="name" id="name" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Awesome Invention Name" aria-describedby="title-description" />
+            <input v-model="formData.name" type="text" name="name" id="name" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-xl border-gray-300 rounded-md" placeholder="Awesome Invention Name" aria-describedby="title-description" />
         </div>
-        <p class="mt-2 text-sm text-gray-500" id="email-description">We'll use this to add help text.</p>
+        <!-- <p class="mt-2 text-sm text-gray-500" id="email-description">We'll use this to add help text.</p> -->
     </div>
     <div>
-        <label for="name" class="block text-sm font-medium text-gray-700">Explain your invention in one line</label>
+        <label for="name" class="block text-2xl font-medium text-gray-700">Explain your invention in one line</label>
         <div class="mt-1">
-            <input v-model="formData.caption" type="text" name="caption" id="caption" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="A shoes that can help you fly" aria-describedby="caption-description" />
+            <input v-model="formData.caption" type="text" name="caption" id="caption" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-xl border-gray-300 rounded-md" placeholder="A shoes that can help you fly" aria-describedby="caption-description" />
         </div>
-        <p class="mt-2 text-sm text-gray-500" id="email-description">We'll use this to add help text.</p>
+        <!-- <p class="mt-2 text-sm text-gray-500" id="email-description">We'll use this to add help text.</p> -->
     </div>
     <div>
-        <label for="description" class="block text-sm font-medium text-gray-700">Describe your invention in a short paragraph</label>
+        <label for="description" class="block text-2xl font-medium text-gray-700">Describe your invention in a short paragraph</label>
         <div class="mt-1">
-            <textarea v-model="formData.description" rows="4" name="description" id="description" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+            <textarea v-model="formData.description" rows="4" name="description" id="description" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-xl border-gray-300 rounded-md" />
         </div>
     </div>
 </div>
