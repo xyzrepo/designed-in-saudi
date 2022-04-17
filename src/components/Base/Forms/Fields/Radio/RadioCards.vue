010d788ecd55c8e95c563427f2912c382e4a62ec
Baker <baker@shamlan.sa>
2022-04-17 11:21:18 +0300

    Update RadioCards.vue

--- src/components/Base/Forms/Fields/Radio/RadioCards.vue
+++ src/components/Base/Forms/Fields/Radio/RadioCards.vue
@@ -4,20 +4,23 @@
         <RadioGroupLabel class="pt-8 text-2xl font-medium text-gray-900">{{label}}</RadioGroupLabel>
 
         <div class="mt-1 pt-4 flex text-xl text-gray-900 sm:mt-0 sm:col-span-2" v-if="selectedOption">
-          <span class="flex-grow">{{selectedOption?.description}}</span>
-          <span class="ml-4 flex-shrink-0">
-            <!-- <button type="button" class="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Update</button> -->
-            <SecondaryButton @click="selectedOption = null">
-                <RefreshIcon class="w-5 h-5"/>
-                Update
-            </SecondaryButton>
-          </span>
+            <slot name="displayField">    
+                <span class="flex-grow">{{selectedOption?.description}}</span>
+                <span class="ml-4 flex-shrink-0">
+                    <!-- <button type="button" class="bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Update</button> -->
+                    <SecondaryButton @click="selectedOption = null">
+                        <RefreshIcon class="w-5 h-5"/>
+                        Update
+                    </SecondaryButton>
+                </span>
+            </slot>
         </div>
-
+        <!-- v-show="selectedOption === null" -->
         <div :class="`mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-${grid ? '2' : '1'} lg:grid-cols-${grid ? '3' : '1'} sm:gap-x-4`" v-show="selectedOption === null">
-            <RadioGroupOption as="span" v-for="option in options" :key="option.id" :value="option" v-slot="{ checked, active }" >
-                <div :class="[checked ? 'border-transparent' : 'border-gray-300', active ? 'border-indigo-500 ring-2 ring-indigo-500' : '', 'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none']" 
-                    @click="$emit('update:modelValue', selectedOption)">
+            <RadioGroupOption as="span" v-for="(option, i) in options" :key="option.id" :value="option" v-slot="{ checked, active }" 
+                @click="$emit('update:modelValue', selectedOption);"
+                :tabindex="i">
+                <div :class="[focus === i ? 'bg-red-700' : '', checked ? 'border-transparent' : 'border-gray-300', active ? 'border-indigo-500 ring-2 ring-indigo-500' : '', 'relative bg-white border rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none']" >
                     <div class="flex-1 flex">
                         <div class="flex flex-col">
                             <RadioGroupLabel v-if="option?.name" as="span" class="block text-xl font-semibold text-gray-900">
@@ -47,7 +50,7 @@ import { CheckCircleIcon } from '@heroicons/vue/solid';
 defineProps({
     grid: {
         type: Boolean,
-        default: true
+        default: false
     },
     label: { 
         type: String,
@@ -64,4 +67,25 @@ defineProps({
 const selectedOption = ref(null)
 // const emit = defineEmits(['update:modelValue'])
 // const update = emit('update:modelValue')
+const focus = ref(null)
+const focusClick = (event,index) => {
+    if (event.keyCode === 'ArrowUp') {
+        if (focus.value === null) {
+            focus.value = 0;
+          } else if (focus.value > 0) {
+            focus.value--;
+          }
+    }
+    else if (event.keyCode === 'ArrowDown') {
+        if (focus.value === null) {
+            focus.value = 0;
+          } else if (focus.value < index + 1) {
+        //   } else {
+            focus.value++;
+          }
+    }
+    // else focus.value = index+1
+    console.log(focus.value, index)
+    // return false;
+}
 </script>
