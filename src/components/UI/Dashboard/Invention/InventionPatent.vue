2777ed8c9647e7d678893398a1c1dbf603d01826
Baker <baker@shamlan.sa>
2022-04-17 11:20:23 +0300

    Update InventionPatent.vue

--- src/components/UI/Dashboard/Invention/InventionPatent.vue
+++ src/components/UI/Dashboard/Invention/InventionPatent.vue
@@ -1,20 +1,9 @@
 <template>
-    <RadioCards 
-        label="Do you have a patent?" 
-        :options="patentOptions" 
-        v-model="formData.patent" :grid="false" 
-    />
-    <ConfirmedInput 
-        label="Patent Number" 
-        v-model="formData.patent_number" 
-        v-if="formData?.patent?.id === 1" 
-    />
-    <RadioCards 
-        label="Have you disclosed your idea publicly or to any third party who is not bound by a confidentiality provision?" 
-        :options="disclosureOptions" 
-        v-model="formData.disclosed" 
-        :grid="false" 
-    />
+    <RadioCards label="Do you have a patent?" :options="patentOptions" v-model="formData.patent" />
+    <ConfirmedInput label="Patent Number" v-model="formData.patent_number" v-if="formData?.patent?.id === 1" />
+    <RadioCards v-model="formData.disclosed" :options="disclosureOptions" 
+        label="Have you disclosed your idea publicly or to any third party who is not bound by a confidentiality provision?">
+    </RadioCards>
 </template>
 
 <script setup>
