97ee275d61c826f7d78e9f935294b83b926ea255
Baker <baker@shamlan.sa>
2022-04-18 05:05:38 +0300

    Update InventionReview2.vue

--- src/components/UI/Dashboard/Invention/InventionReview2.vue
+++ src/components/UI/Dashboard/Invention/InventionReview2.vue
@@ -28,7 +28,7 @@
                         <dt class="text-md font-medium text-gray-500">Elevator Pitch</dt>
                         <dd v-if="data.caption" class="mt-1 text-md text-gray-900">{{ data.caption }}</dd>
                     </div>
-                    <div  class="sm:col-span-2">
+                    <div class="sm:col-span-2">
                         <dt class="text-md font-medium text-gray-500">Description</dt>
                         <dd v-if="data.description" class="mt-1 text-md text-gray-900">{{ data.description }}</dd>
                     </div>
@@ -37,9 +37,15 @@
                         <dd v-if="data.patent" class="mt-1 text-md text-gray-900">
                             {{
                                 data.patent?.id === 1 ? 'Yes, Patent Number ' + data.patent_number
-                                    : data.patent?.id === 2 ? 'Pending'
-                                        : 'No'
+                                : data.patent?.id === 2 ? 'Pending'
+                                    : 'No'
                             }}
+                        </dd>
+                    </div>
+                    <div class="sm:col-span-2">
+                        <dt class="text-md font-medium text-gray-500">Disclosure Status</dt>
+                        <dd v-if="data.disclosed" class="mt-1 text-md text-gray-900">
+                            {{ data.disclosed.description }}
                         </dd>
                     </div>
                     <div class="sm:col-span-2">
@@ -67,26 +73,10 @@
                             </ul>
                         </dd>
                     </div>
-                    <!-- <div class="sm:col-span-2">
-                        <dt class="text-sm font-medium text-gray-500">Attachments</dt>
-                        <dd class="mt-1 text-sm text-gray-900">
-                            <ul role="list" class="border border-gray-200 rounded-md divide-y divide-gray-200">
-                                <li v-for="attachment in attachments" :key="attachment.name"
-                                    class="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
-                                    <div class="w-0 flex-1 flex items-center">
-                                        <PaperClipIcon class="flex-shrink-0 h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        <span class="ml-2 flex-1 w-0 truncate">
-                                            {{ attachment.name }}
-                                        </span>
-                                    </div>
-                                    <div class="ml-4 flex-shrink-0">
-                                        <a :href="attachment.href"
-                                            class="font-medium text-blue-600 hover:text-blue-500"> Download </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </dd>
-                    </div> -->
+
+                    <div class="sm:col-span-2">
+                        <InventionAttachments />
+                    </div>
                 </dl>
             </div>
         </div>
@@ -94,6 +84,7 @@
 </template>
 
 <script setup>
+import InventionAttachments from './InventionAttachments.vue';
 // import { formData as data } from '@composables/useInvention'
 defineProps({ data: Object })
 </script>
