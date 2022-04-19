b62f1b75e7d5ac23b11941e521440c07b595cc6c
Baker <baker@shamlan.sa>
2022-04-19 06:17:03 +0300

    Update InventionUI.vue

--- src/components/UI/Dashboard/Invention/InventionUI.vue
+++ src/components/UI/Dashboard/Invention/InventionUI.vue
@@ -1,43 +1,45 @@
 <template>
-<InventionWizard :steps="steps">
-    <!-- <Title>
-    {{ steps[step]?.name || 'Complete' }}
-    </Title> -->
-    <span id="top"></span>
-    
-    <InventionStep :step="0" title="Ready to invent?" subtitle="Start creating your first invention today." class="text-center pt-24"
-        :action="{ name: `Let's get started` , fn: nextStep }" />
-    
-    <InventionStep :step="1" title="Invention Information" subtitle="Tell us a little bit about your invention.">
-       <InventionForm />
-    </InventionStep>
+    <InventionWizard :steps="steps">
+        <!-- <Title>
+                {{ steps[step]?.name || 'Complete' }}
+                </Title> -->
+        <span id="top"></span>
 
-    <InventionStep :step="2" title="Category" :action="{ name: `Continue`, fn: nextStep }">
-        <RadioCards v-model="formData.category" :grid="true" label="Category" :options="categories" />
-        <RadioCards v-if="formData.category?.subcategories?.length > 0" v-model="formData.subcategory" 
-        :grid="true" label="Subcategory" :options="formData.category.subcategories" />
-    </InventionStep>
+        <InventionStep :step="0" title="Ready to invent?" subtitle="Start creating your first invention today."
+            class="pt-24 text-center" :action="{ name: `Let's get started`, fn: nextStep }" />
 
-    <InventionStep :step="3" title="Files and Resources" subtitle="Inspirations, sketches, prototypes, renders, and videos">
-        <!-- <InventionPatent /> -->
-    </InventionStep>
+        <InventionStep :step="1" title="Invention Information" subtitle="Tell us a little bit about your invention.">
+            <InventionForm />
+        </InventionStep>
 
-    <InventionStep :step="4" title="Patent and Disclosure" subtitle="invention.welcome.subtitle #4">
-        <InventionPatent />
-    </InventionStep>
+        <InventionStep :step="2" title="Category" :action="{ name: `Continue`, fn: nextStep }">
+            <RadioCards v-model="formData.category" :grid="true" label="Category" :options="categories" />
+            <RadioCards v-if="formData.category?.subcategories?.length > 0" v-model="formData.subcategory" :grid="true"
+                label="Subcategory" :options="formData.category.subcategories" />
+        </InventionStep>
 
-    <InventionStep :step="5" title="Privacy Setting" subtitle="Please choose the privacy level you require">
-        <RadioCards v-model="formData.privacy_level" label="Privacy" :options="privacyOptions" />
-    </InventionStep>
+        <InventionStep :step="3" title="Files and Resources"
+            subtitle="Inspirations, sketches, prototypes, renders, and videos">
+            <!-- <InventionPatent /> -->
+        </InventionStep>
 
-    <InventionStep :step="6" title="Review Your Submission" :action="{ name: 'Submit', fn: () => submit }">
-        <InventionReview :data="formData" />
-    </InventionStep>
-    <span id="bottom"></span>
-</InventionWizard>
+        <InventionStep :step="4" title="Patent and Disclosure" subtitle="invention.welcome.subtitle #4">
+            <InventionPatent />
+        </InventionStep>
+
+        <InventionStep :step="5" title="Privacy Setting" subtitle="Please choose the privacy level you require">
+            <RadioCards v-model="formData.privacy_level" label="Privacy" :options="privacyOptions" />
+        </InventionStep>
+
+        <InventionStep :step="6" title="Review Your Submission" :action="{ name: 'Submit', fn: () => submit }">
+            <InventionReview :data="formData" />
+        </InventionStep>
+        <span id="bottom"></span>
+    </InventionWizard>
 </template>
+
 <script setup>
-import { formData, categories, nextStep, privacyOptions } from '@composables/useInvention';
+import { formData, categories, nextStep, privacyOptions } from '@composables/useInvention'
 const steps = ref([
     { name: 'Step 1' },
     { name: 'Step 2' },
