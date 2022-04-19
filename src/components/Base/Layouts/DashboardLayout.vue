e99da5a1eb6c8639b1981c7eeab6ae73335efb1b
Baker <baker@shamlan.sa>
2022-04-19 05:41:22 +0300

    Update DashboardLayout.vue

--- src/components/Base/Layouts/DashboardLayout.vue
+++ src/components/Base/Layouts/DashboardLayout.vue
@@ -1,87 +1,113 @@
 <template>
   <div>
     <TransitionRoot as="template" :show="sidebarOpen">
-      <Dialog as="div" class="fixed inset-0 flex z-40 md:hidden" @close="sidebarOpen = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100" leave-to="opacity-0">
+      <Dialog as="div" class="fixed inset-0 z-40 flex md:hidden" @close="sidebarOpen = false">
+        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+          enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
+          leave-to="opacity-0">
           <DialogOverlay class="fixed inset-0 bg-gray-600 bg-opacity-75" />
         </TransitionChild>
-        <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-          <div class="relative flex-1 flex flex-col max-w-xs w-full bg-white">
-            <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
+        <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
+          enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform"
+          leave-from="translate-x-0" leave-to="-translate-x-full">
+          <div class="relative flex w-full max-w-xs flex-1 flex-col bg-white">
+            <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+              enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
               <div class="absolute top-0 right-0 -mr-12 pt-2">
-                <button type="button" class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
+                <button type="button"
+                  class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  @click="sidebarOpen = false">
                   <span class="sr-only">Close sidebar</span>
                   <XIcon class="h-6 w-6 text-white" aria-hidden="true" />
                 </button>
               </div>
             </TransitionChild>
-            <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-              <div class="flex-shrink-0 flex items-center px-4">
+            <div class="h-0 flex-1 overflow-y-auto pt-5 pb-4">
+              <div class="flex flex-shrink-0 items-center px-4">
                 <img class="h-8 w-auto" src="/logo.png" alt="logo" />
               </div>
-              <nav class="mt-5 px-2 space-y-1">
+              <nav class="mt-5 space-y-1 px-2">
                 <!-- <route-link tag="a" v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']"> -->
-                <router-link tag="a" v-for="item in navigation" :key="item.name" :to="item.href" class="active:bg-gray-100 active:text-gray-900 text-gray-600 hover:bg-gray-50 hover:text-primary-900 group flex items-center px-2 py-2 text-base font-semibold rounded-md">
+                <router-link v-for="item in navigation" :key="item.name" :to="item.href"
+                  class="group flex items-center rounded-md px-2 py-2 text-base font-semibold text-gray-600 hover:bg-gray-50 hover:text-primary-900 active:bg-gray-100 active:text-gray-900">
                   <!-- <component :is="item.icon" :class="[item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500', 'mr-4 flex-shrink-0 h-6 w-6']" aria-hidden="true" /> -->
-                  <component :is="item.icon" class="active:text-gray-500 text-gray-400 group-hover:text-gray-500 mr-4 flex-shrink-0 h-6 w-6" aria-hidden="true" />
+                  <component :is="item.icon"
+                    class="mr-4 h-6 w-6 flex-shrink-0 text-gray-400 active:text-gray-500 group-hover:text-gray-500"
+                    aria-hidden="true" />
                   {{ item.name }}
                 </router-link>
               </nav>
             </div>
-            <div class="flex-shrink-0 flex border-t border-gray-200 p-4">
-              <a href="/dashboard/profile" class="flex-shrink-0 group block">
+            <div class="flex flex-shrink-0 border-t border-gray-200 p-4">
+              <a href="/dashboard/profile" class="group block flex-shrink-0">
                 <div class="flex items-center">
                   <div>
-                    <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                    <img class="inline-block h-10 w-10 rounded-full"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt="" />
                   </div>
                   <div class="ml-3">
-                    <p class="text-base font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                    <p class="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
+                    <p class="text-base font-medium text-gray-700 group-hover:text-gray-900">
+                      Tom Cook
+                    </p>
+                    <p class="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+                      View profile
+                    </p>
                   </div>
                 </div>
               </a>
             </div>
           </div>
         </TransitionChild>
-        <div class="flex-shrink-0 w-14">
+        <div class="w-14 flex-shrink-0">
           <!-- Force sidebar to shrink to fit close icon -->
         </div>
       </Dialog>
     </TransitionRoot>
 
     <!-- Static sidebar for desktop -->
-    <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+    <div class="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white">
-        <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-          <div class="flex items-center flex-shrink-0 px-4">
+      <div class="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
+        <div class="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
+          <div class="flex flex-shrink-0 items-center px-4">
             <img class="h-20 w-auto" src="/logo.png" alt="logo" />
           </div>
-          <nav class="mt-5 flex-1 px-2 bg-white space-y-1">
-            <router-link tag="a" v-for="item in navigation" :key="item.name" :to="item.href" class="active:bg-gray-100 active:text-gray-900 text-gray-600 hover:bg-gray-50 hover:text-primary-900 group flex items-center px-2 py-2 text-base font-semibold rounded-md">
-              <component :is="item.icon" class="group-hover:text-gray-500 mr-3 flex-shrink-0 h-6 w-6" aria-hidden="true" />
+          <nav class="mt-5 flex-1 space-y-1 bg-white px-2">
+            <router-link v-for="item in navigation" :key="item.name" :to="item.href"
+              class="group flex items-center rounded-md px-2 py-2 text-base font-semibold text-gray-600 hover:bg-gray-50 hover:text-primary-900 active:bg-gray-100 active:text-gray-900">
+              <component :is="item.icon" class="mr-3 h-6 w-6 flex-shrink-0 group-hover:text-gray-500"
+                aria-hidden="true" />
               {{ item.name }}
             </router-link>
           </nav>
         </div>
-        <div class="flex-shrink-0 flex border-t border-gray-200 p-4">
-          <a href="/dashboard/profile" class="flex-shrink-0 w-full group block">
+        <div class="flex flex-shrink-0 border-t border-gray-200 p-4">
+          <a href="/dashboard/profile" class="group block w-full flex-shrink-0">
             <div class="flex items-center">
               <div>
-                <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <img class="inline-block h-9 w-9 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt="" />
               </div>
               <div class="ml-3">
-                <p class="text-sm font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
+                <p class="text-sm font-medium text-gray-700 group-hover:text-gray-900">
+                  Tom Cook
+                </p>
+                <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700">
+                  View profile
+                </p>
               </div>
             </div>
           </a>
         </div>
       </div>
     </div>
-    <div class="md:pl-64 flex flex-col flex-1">
-      <div class="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
-        <button type="button" class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="sidebarOpen = true">
+    <div class="flex flex-1 flex-col md:pl-64">
+      <div class="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
+        <button type="button"
+          class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          @click="sidebarOpen = true">
           <span class="sr-only">Open sidebar</span>
           <MenuIcon class="h-6 w-6" aria-hidden="true" />
         </button>
@@ -89,15 +115,15 @@
       <main class="flex-1">
         <div class="py-6">
           <!-- <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-            <h1 class="text-2xl font-semibold text-gray-900">{{ title || 'Dashboard' }}</h1>
-          </div> -->
-          <div class="max-w-7xl mx-auto px-2 sm:px-3 md:px-4">
+                    <h1 class="text-2xl font-semibold text-gray-900">{{ title || 'Dashboard' }}</h1>
+                  </div> -->
+          <div class="mx-auto max-w-7xl px-2 sm:px-3 md:px-4">
             <!-- Replace with your content -->
-            <div class="py-4">
-              <slot>
-                <div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-              </slot>
-            </div>
+            <slot>
+              <div class="py-4">
+                <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+              </div>
+            </slot>
             <!-- /End replace -->
           </div>
         </div>
@@ -107,7 +133,12 @@
 </template>
 
 <script setup>
-import { Dialog, DialogOverlay, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import {
+  Dialog,
+  DialogOverlay,
+  TransitionChild,
+  TransitionRoot
+} from '@headlessui/vue'
 import {
   CalendarIcon,
   ChartBarIcon,
@@ -116,9 +147,9 @@ import {
   InboxIcon,
   MenuIcon,
   UsersIcon,
-  XIcon,
+  XIcon
 } from '@heroicons/vue/outline'
-import content from '@content';
+import content from '@content'
 const navigation = content.dashboard.navigation
 // const navigation = [
 //   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
