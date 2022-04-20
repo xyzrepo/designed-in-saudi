0eae6166794ab88334904ccab81d93732f0e1034
Baker <baker@shamlan.sa>
2022-04-20 05:22:48 +0300

    Update PageHeadingActions.vue

--- src/components/Base/Headings/PageHeadings/PageHeadingActions.vue
+++ src/components/Base/Headings/PageHeadings/PageHeadingActions.vue
@@ -1,36 +1,22 @@
 <template>
-    <div class="mt-5 flex lg:mt-0 lg:ml-4">
-        <span
-            v-for="(item, index) in actions"
-            :key="index"
-            class="z-10 block"
-            :class="{ 'ml-3': !!index }">
-            <button
-                v-if="item.primary"
-                type="button"
-                class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                @click="item.action">
-                <component
-                    :is="item.icon"
-                    class="-ml-1 mr-2 h-5 w-5"
-                    aria-hidden="true" />
-                {{ item.name }}
-            </button>
-            <button
-                v-else
-                type="button"
-                class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                @click="item.action">
-                <component
-                    :is="item.icon"
-                    class="-ml-1 mr-2 h-5 w-5 text-gray-500"
-                    aria-hidden="true" />
-                {{ item.name }}
-            </button>
-        </span>
+  <div class="mt-5 flex lg:mt-0 lg:ml-4">
+    <span v-for="(item, index) in actions" :key="index" class="z-10 block" :class="{ 'ml-3': !!index }">
+      <button v-if="item.primary" type="button"
+        class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        @click="item.action">
+        <component :is="item.icon" class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+        {{ item.name }}
+      </button>
+      <button v-else type="button"
+        class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        @click="item.action">
+        <component :is="item.icon" class="-ml-1 mr-2 h-5 w-5 text-gray-500" aria-hidden="true" />
+        {{ item.name }}
+      </button>
+    </span>
 
-        <!-- Dropdown -->
-        <!-- <Menu as="span" class="ml-3 relative sm:hidden z-10">
+    <!-- Dropdown -->
+    <!-- <Menu as="span" class="ml-3 relative sm:hidden z-10">
         <MenuButton class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Actions
           <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5 text-gray-500" aria-hidden="true" />
@@ -44,7 +30,7 @@
           </MenuItems>
         </transition>
       </Menu> -->
-    </div>
+  </div>
 </template>
 <script setup>
 import { ChevronDownIcon } from '@heroicons/vue/outline'
