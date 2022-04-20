a8cc169f5c32b71efb3d2978317cbb342e1e35fb
Baker <baker@shamlan.sa>
2022-04-20 05:22:21 +0300

    Update Terms.vue

--- src/pages/Legal/Terms.vue
+++ src/pages/Legal/Terms.vue
@@ -15,105 +15,53 @@
   ```
 -->
 <template>
-    <div class="relative overflow-hidden bg-white py-16">
-        <div
-            class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full">
-            <div
-                class="relative mx-auto h-full max-w-prose text-lg"
-                aria-hidden="true">
-                <svg
-                    class="absolute top-12 left-full translate-x-32 transform"
-                    width="404"
-                    height="384"
-                    fill="none"
+    <div class="relative overflow-hidden bg-gray-50 py-16">
+        <div class="hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full">
+            <div class="relative mx-auto h-full max-w-prose text-lg" aria-hidden="true">
+                <svg class="absolute top-12 left-full translate-x-32 transform" width="404" height="384" fill="none"
                     viewBox="0 0 404 384">
                     <defs>
-                        <pattern
-                            id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-                            x="0"
-                            y="0"
-                            width="20"
-                            height="20"
+                        <pattern id="74b3fd99-0a6f-4271-bef2-e80eeafdf357" x="0" y="0" width="20" height="20"
                             patternUnits="userSpaceOnUse">
-                            <rect
-                                x="0"
-                                y="0"
-                                width="4"
-                                height="4"
-                                class="text-gray-200"
-                                fill="currentColor" />
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
                         </pattern>
                     </defs>
-                    <rect
-                        width="404"
-                        height="384"
-                        fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+                    <rect width="404" height="384" fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
                 </svg>
-                <svg
-                    class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform"
-                    width="404"
-                    height="384"
-                    fill="none"
-                    viewBox="0 0 404 384">
+                <svg class="absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform" width="404"
+                    height="384" fill="none" viewBox="0 0 404 384">
                     <defs>
-                        <pattern
-                            id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                            x="0"
-                            y="0"
-                            width="20"
-                            height="20"
+                        <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20"
                             patternUnits="userSpaceOnUse">
-                            <rect
-                                x="0"
-                                y="0"
-                                width="4"
-                                height="4"
-                                class="text-gray-200"
-                                fill="currentColor" />
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
                         </pattern>
                     </defs>
-                    <rect
-                        width="404"
-                        height="384"
-                        fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+                    <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
                 </svg>
-                <svg
-                    class="absolute bottom-12 left-full translate-x-32 transform"
-                    width="404"
-                    height="384"
-                    fill="none"
+                <svg class="absolute bottom-12 left-full translate-x-32 transform" width="404" height="384" fill="none"
                     viewBox="0 0 404 384">
                     <defs>
-                        <pattern
-                            id="d3eb07ae-5182-43e6-857d-35c643af9034"
-                            x="0"
-                            y="0"
-                            width="20"
-                            height="20"
+                        <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20"
                             patternUnits="userSpaceOnUse">
-                            <rect
-                                x="0"
-                                y="0"
-                                width="4"
-                                height="4"
-                                class="text-gray-200"
-                                fill="currentColor" />
+                            <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
                         </pattern>
                     </defs>
-                    <rect
-                        width="404"
-                        height="384"
-                        fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+                    <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
                 </svg>
             </div>
         </div>
-        <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-prose text-lg w-full prose">
+            <TermsContent />
+        </div>
+        <!-- <div class="relative px-4 sm:px-6 lg:px-8">
+            
             <div class="mx-auto max-w-prose text-lg">
                 <h1>
                     <span
                         class="block text-center text-base font-semibold uppercase tracking-wide text-indigo-600">Introducing</span>
                     <span
-                        class="mt-2 block text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">JavaScript for Beginners</span>
+                        class="mt-2 block text-center text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">JavaScript
+                        for Beginners</span>
                 </h1>
                 <p class="mt-8 text-xl leading-8 text-gray-500">
                     Aliquet nec orci mattis amet quisque ullamcorper neque, nibh
@@ -175,12 +123,9 @@
                     viverra tellus varius sit neque erat velit.
                 </p>
                 <figure>
-                    <img
-                        class="w-full rounded-lg"
+                    <img class="w-full rounded-lg"
                         src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-                        alt=""
-                        width="1310"
-                        height="873" />
+                        alt="" width="1310" height="873" />
                     <figcaption>
                         Sagittis scelerisque nulla cursus in enim consectetur
                         quam.
@@ -203,6 +148,6 @@
                     viverra tellus varius sit neque erat velit.
                 </p>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
